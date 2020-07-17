05b896b8fd071931e993da3b83b0fc2241aadfaa
Michelly-Oliveira <michellyneris.oliveira@gmail.com>
2020-07-17 15:03:24 -0300

    Updates

---
+++ precache-manifest.75ea5935b1c614956e33e8ec20cf8c22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2739151a4316445e5bbee1f0018f0afc",
+    "url": "/lotr-project/index.html"
+  },
+  {
+    "revision": "13b42c5a4e4bd988feea",
+    "url": "/lotr-project/static/js/2.4ec43142.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lotr-project/static/js/2.4ec43142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d50f7ae77d3c820333",
+    "url": "/lotr-project/static/js/main.af8e77f7.chunk.js"
+  },
+  {
+    "revision": "38a5dbbf4cf7d165cbcf",
+    "url": "/lotr-project/static/js/runtime-main.f0acc270.js"
+  },
+  {
+    "revision": "753b5e522d2820227cac7d25067441e3",
+    "url": "/lotr-project/static/media/loadingRing.753b5e52.gif"
+  },
+  {
+    "revision": "d265f3186ec1092d7cf0c46c45c5b5b2",
+    "url": "/lotr-project/static/media/logo.d265f318.png"
+  }
+]);
